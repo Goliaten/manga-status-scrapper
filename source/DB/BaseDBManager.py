27821672ca056d8b60d9f7955d76bb7d1655353e
Goliaten <ognikat4@gmail.com>
2025-10-20 23:46:02 +0200

    temprorarily neuter base db manager

--- source/DB/BaseDBManager.py
+++ source/DB/BaseDBManager.py
@@ -1,21 +1,22 @@
 from abc import abstractmethod
 from typing import Any, Dict, List, Tuple
-from source.data_classes.ScrapResult import ScrapResult
+# from .data_classes.ScrapResult import ScrapResult
 
 
 class BaseDBManager:
-    @abstractmethod
-    def insert_scrap_results(
-        self, scrap_results: List[ScrapResult]
-    ) -> Tuple[int, str]: ...
+    pass
+    # @abstractmethod
+    # def insert_scrap_results(
+    #     self, scrap_results: List[ScrapResult]
+    # ) -> Tuple[int, str]: ...
 
-    @abstractmethod
-    def get_scrap_results(self, filters: Dict[str, Any]) -> List[ScrapResult]: ...
+    # @abstractmethod
+    # def get_scrap_results(self, filters: Dict[str, Any]) -> List[ScrapResult]: ...
 
-    @abstractmethod
-    def upsert_scrap_results(
-        self, scrap_results: List[ScrapResult]
-    ) -> Tuple[int, str]: ...
+    # @abstractmethod
+    # def upsert_scrap_results(
+    #     self, scrap_results: List[ScrapResult]
+    # ) -> Tuple[int, str]: ...
 
-    @abstractmethod
-    def deactivate_scrap_results(self, filters: Dict[str, Any]) -> Tuple[int, str]: ...
+    # @abstractmethod
+    # def deactivate_scrap_results(self, filters: Dict[str, Any]) -> Tuple[int, str]: ...
